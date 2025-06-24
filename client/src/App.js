fcdd3fe5ae101e94c3dea08458ca1ba50c72a2c5
NoProjectGames <npgamesstudio@gmail.com>
2025-06-24 14:08:03 +0200

    testsocket adresse

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://jeunavtest.onrender.com');
 
 const NB_PLAYERS = 8;
 const BASE_VIEW_RATIO = 0.7;
